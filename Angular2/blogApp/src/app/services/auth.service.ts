f483c8d28464d3574d56228c1cd66bcb2d2c3164
shahlabs <labdhi0601@gmail.com>
2018-03-02 12:10:24 -0500

    login

---
+++ Angular2/blogApp/src/app/services/auth.service.ts
@@ -0,0 +1,49 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/map';
+import { tokenNotExpired } from 'angular2-jwt';
+
+@Injectable()
+export class AuthService {
+
+    domain = "http://localhost:8080"; // Development Domain - Not Needed in Production
+    authToken;
+    user;
+    options;
+
+    constructor(
+      private http: Http
+    ) { }
+
+    // Function to register user accounts
+    registerUser(user) {
+      return this.http.post(this.domain + '/authentication/register', user).map(res => res.json());
+    }
+
+    // Function to check if username is taken
+    checkUsername(username) {
+      return this.http.get(this.domain + '/authentication/checkUsername/' + username).map(res => res.json());
+    }
+
+    // Function to check if e-mail is taken
+    checkEmail(email) {
+      return this.http.get(this.domain + '/authentication/checkEmail/' + email).map(res => res.json());
+    }
+
+  storeUserData(token, user) {
+    localStorage.setItem('token', token); // Set token in local storage
+    localStorage.setItem('user', JSON.stringify(user)); // Set user in local storage as string
+    this.authToken = token; // Assign token to be used elsewhere
+    this.user = user; // Set user to be used elsewhere
+  }
+
+  login(user) {
+  console.log("In auth.service");
+    return this.http.post(this.domain + 'authentication/login', user).map(res => res.json());
+  }
+
+  loggedIn() {
+    return tokenNotExpired();
+  }
+
+}
